641977e39557ea9b7d5f6a1b8d14c6bcc34aeb36
Bryan Terce <bryan.terce@gmail.com>
2020-09-30 22:25:40 -0700

    Add theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,4 +4,19 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
+  theme: {
+    extend: {
+      colors: {
+        sea: {
+          light: "#32a09d",
+          dark: "#2c819e",
+        },
+        paper: "#ddcfba",
+        brown: {
+          200: "#af9d87",
+          900: "#4e382c",
+        },
+      },
+    },
+  },
 };
