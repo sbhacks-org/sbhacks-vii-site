cde3da58375b58f449f7e010c7a2b9ba884ff4c8
Bryan Terce <bryan.terce@gmail.com>
2020-10-10 18:02:33 -0700

    Add event and mailing list assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,24 @@
+const assetWidths = {};
+
+function defineAsset(name, widthPx) {
+  assetWidths[name] = pxToEm(widthPx);
+}
+
+function pxToEm(px) {
+  return `${truncateEm(px / 16)}em`;
+}
+
+function truncateEm(em) {
+  return em.toFixed(3).replace(/\.?0+$/, "");
+}
+
+defineAsset("logo_map_cloud", 198);
+defineAsset("cloud_front", 180);
+defineAsset("cloud_back", 101);
+defineAsset("island", 133);
+defineAsset("envelope", 114);
+defineAsset("wax_seal_submit", 38);
+
 module.exports = {
   plugins: [
     require("@tailwindcss/ui"),
@@ -23,6 +44,9 @@ module.exports = {
       },
       fontFamily: {
         sans: ["'Nexa Bold'"],
+      },
+      width: {
+        ...assetWidths,
       },
     },
   },
