a978e538686c992d152cf69d7e29e0c525a4b768
Tina Shi <tshi@umail.ucsb.edu>
2020-11-01 12:12:49 -0800

    added apply buttons and otter

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,9 @@ defineAsset("cloud_back", 101);
 defineAsset("island", 133);
 defineAsset("envelope", 114);
 defineAsset("wax_seal_submit", 38);
+defineAsset("wheel", 27);
+defineAsset("anchor", 27);
+defineAsset("sailor_otter_telescope", 38);
 
 module.exports = {
   plugins: [
@@ -41,6 +44,10 @@ module.exports = {
         brown: {
           200: "#af9d87",
           900: "#4e382c",
+        },
+        button: {
+          red: "#d37676",
+          blue: "#316cb0",
         },
       },
       fontFamily: {
